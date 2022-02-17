fad567aa1b409757adb65d3e913d7fdf5349ee23
simasbakus <simas.bakus@gmail.com>
2022-02-17 08:03:49 +0200

    Initial commit

---
+++ SchedulerWebApi/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchedulerWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var test = _context.Users.ToList();
+            return Ok("Success");
+        }
+    }
+}
---
+++ SchedulerWebApi/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SchedulerWebApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
